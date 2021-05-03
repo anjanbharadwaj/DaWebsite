0d76c7ddf67732fbfabfc624bc040d3ae71ea0bb
= <anjanbharadwaj02@gmail.com>
2021-05-03 16:51:18 -0700

    Basic mouse and keyboard access setup for 4 basic sounds, styling remains to be done

--- dapiano/src/Components/Piano/Piano.tsx
+++ dapiano/src/Components/Piano/Piano.tsx
@@ -1,13 +1,61 @@
 import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import PianoKey from "../PianoKey/PianoKey";
 import styles from "../Piano/Piano.module.css";
+import useSound from "use-sound";
+import KeyboardEventHandler from "react-keyboard-event-handler";
+
 const Piano = () => {
+  const filePathA: string = "/audio/letsgo.mp3";
+  const [playA] = useSound(filePathA, { volume: 0.25 });
+
+  const filePathB: string = "/audio/ha.mp3";
+  const [playB] = useSound(filePathB, { volume: 0.25 });
+
+  const filePathC: string = "/audio/yeahyeah.mp3";
+  const [playC] = useSound(filePathC, { volume: 0.25 });
+
+  const filePathD: string = "/audio/ipullup.mp3";
+  const [playD] = useSound(filePathD, { volume: 0.25 });
+  const handleKeyPress = (key: string) => {
+    switch (key) {
+      case "a":
+        playA();
+        break;
+      case "b":
+        playB();
+        break;
+      case "c":
+        playC();
+        break;
+      case "d":
+        playD();
+        break;
+    }
+    if (key === "a") {
+      playA();
+    }
+  };
+
   return (
     <div>
-      <PianoKey soundEffect={"lets go"}></PianoKey>
-      <PianoKey soundEffect={"hah"}></PianoKey>
-      <PianoKey soundEffect={"yeah"}></PianoKey>
-      <PianoKey soundEffect={"no cap"}></PianoKey>
+      <KeyboardEventHandler
+        handleKeys={["a", "b", "c", "d"]}
+        onKeyEvent={(key: any, e: any) => handleKeyPress(key)}
+      />
+      <div className={styles.container}>
+        <PianoKey keyToPress={"a"} soundEffect={"letsgo"}></PianoKey>
+        <PianoKey keyToPress={"b"} soundEffect={"ha"}></PianoKey>
+        <PianoKey keyToPress={"c"} soundEffect={"yeahyeah"}></PianoKey>
+        <PianoKey keyToPress={"d"} soundEffect={"ipullup"}></PianoKey>
+      </div>
+      <div className={styles.footerContainer}>
+        <p>Created with 🎶 and 🥁 by Anjan Bharadwaj</p>
+        <p>
+          Check out this project on{" "}
+          <a href="https://github.com/anjanbharadwaj/DaWebsite">GitHub</a>
+        </p>
+      </div>
     </div>
   );
 };
--- dapiano/src/Components/PianoKey/PianoKey.tsx
+++ dapiano/src/Components/PianoKey/PianoKey.tsx
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
+import useSound from "use-sound";
+import styles from "../PianoKey/PianoKey.module.css";
 
 interface KeyProps {
   soundEffect: string;
@@ -6,7 +8,48 @@ interface KeyProps {
 }
 
 const PianoKey = (props: KeyProps) => {
-  return <h1>{props.soundEffect}</h1>;
+  let pianoOptions = {
+    volume: 0.5,
+    interrupt: false,
+  };
+  const filePath: string = "/audio/" + props.soundEffect + ".mp3";
+  const [play] = useSound(filePath, { volume: 0.25 });
+  // const handleKeyPress = (event: any) => {
+  //   if (event.key === props.keyToPress) {
+  //     console.log(event.key, props.keyToPress);
+  //     play();
+  //   }
+  // };
+  // useEffect(() => {
+  //   console.log("running use effect");
+  //   window.addEventListener("keypress", handleKeyPress);
+  // }, []);
+
+  // window.addEventListener("keypress", (event: any) => {
+  //   if (event.key === props.keyToPress) {
+  //     console.log(event.key, props.keyToPress);
+  //     play();
+  //   }
+  // });
+  // const filePath: string = "/audio/letsgo.mp3";
+  // const [play] = useSound(filePath, { volume: 0.5 });
+  // React.useEffect(() => {
+  //   window.addEventListener("keypress", (event) => {
+  //     if (event.key === "a") {
+  //       console.log("enter press here! ");
+  //       play();
+  //     }
+  //   });
+  // }, []);
+
+  return (
+    <div className={styles.container}>
+      <button className={styles.whiteTile} onClick={() => play()}>
+        {props.keyToPress}
+      </button>
+    </div>
+  );
+  // return <h1>{props.soundEffect}</h1>;
 };
 
 export default PianoKey;
