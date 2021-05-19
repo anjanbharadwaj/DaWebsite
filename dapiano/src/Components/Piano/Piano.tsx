fbbde11f848c82a48cb29ef8a994cd6956bca8b8
= <anjanbharadwaj02@gmail.com>
2021-05-19 00:24:33 -0700

    Fixing interruptions on useSound, more styling

--- dapiano/src/Components/Piano/Piano.tsx
+++ dapiano/src/Components/Piano/Piano.tsx
@@ -6,32 +6,36 @@ import useSound from "use-sound";
 import KeyboardEventHandler from "react-keyboard-event-handler";
 
 const Piano = () => {
+  let pianoOptions = {
+    volume: 0.25,
+    interrupt: true,
+  };
   const filePathA: string = "/audio/letsgo.mp3";
-  const [playA] = useSound(filePathA, { volume: 0.25 });
+  const [playA] = useSound(filePathA, pianoOptions);
 
   const filePathB: string = "/audio/ha.mp3";
-  const [playB] = useSound(filePathB, { volume: 0.25 });
+  const [playB] = useSound(filePathB, pianoOptions);
 
   const filePathC: string = "/audio/yeahyeah.mp3";
-  const [playC] = useSound(filePathC, { volume: 0.25 });
+  const [playC] = useSound(filePathC, pianoOptions);
 
   const filePathD: string = "/audio/ipullup.mp3";
-  const [playD] = useSound(filePathD, { volume: 0.25 });
+  const [playD] = useSound(filePathD, pianoOptions);
 
   const filePathE: string = "/audio/nocap.mp3";
-  const [playE] = useSound(filePathE, { volume: 0.25 });
+  const [playE] = useSound(filePathE, pianoOptions);
 
   const filePathF: string = "/audio/youknowitsbaby.mp3";
-  const [playF] = useSound(filePathF, { volume: 0.25 });
+  const [playF] = useSound(filePathF, pianoOptions);
 
   const filePathG: string = "/audio/ha2.mp3";
-  const [playG] = useSound(filePathG, { volume: 0.25 });
+  const [playG] = useSound(filePathG, pianoOptions);
 
   const filePathH: string = "/audio/hot.mp3";
-  const [playH] = useSound(filePathH, { volume: 0.25 });
+  const [playH] = useSound(filePathH, pianoOptions);
 
   const filePathI: string = "/audio/okay.mp3";
-  const [playI] = useSound(filePathI, { volume: 0.25 });
+  const [playI] = useSound(filePathI, pianoOptions);
   const handleKeyPress = (key: string) => {
     switch (key) {
       case "a":
@@ -65,34 +69,45 @@ const Piano = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <KeyboardEventHandler
-        handleKeys={["a", "b", "c", "d", "e", "f", "g", "h", "i"]}
-        onKeyEvent={(key: any, e: any) => handleKeyPress(key)}
-      />
-      <PianoKey tileType={1} keyToPress={"a"} soundEffect={"letsgo"}></PianoKey>
-      <PianoKey tileType={1} keyToPress={"b"} soundEffect={"ha"}></PianoKey>
-      <PianoKey
-        tileType={1}
-        keyToPress={"c"}
-        soundEffect={"yeahyeah"}
-      ></PianoKey>
-      <PianoKey
-        tileType={1}
-        keyToPress={"d"}
-        soundEffect={"ipullup"}
-      ></PianoKey>
-      <PianoKey tileType={1} keyToPress={"e"} soundEffect={"nocap"}></PianoKey>
-      <PianoKey
-        tileType={2}
-        keyToPress={"f"}
-        soundEffect={"youknowitsbaby"}
-      ></PianoKey>
-      <PianoKey tileType={2} keyToPress={"g"} soundEffect={"ha2"}></PianoKey>
-      <PianoKey tileType={2} keyToPress={"h"} soundEffect={"hot"}></PianoKey>
-      <PianoKey tileType={2} keyToPress={"i"} soundEffect={"okay"}></PianoKey>
+    <div className={styles.mainContainer}>
+      <div className={styles.whiteContainer}>
+        <KeyboardEventHandler
+          handleKeys={["a", "b", "c", "d", "e", "f", "g", "h", "i"]}
+          onKeyEvent={(key: any, e: any) => handleKeyPress(key)}
+        />
+        <PianoKey
+          tileType={1}
+          keyToPress={"a"}
+          soundEffect={"letsgo"}
+        ></PianoKey>
+        <PianoKey tileType={1} keyToPress={"b"} soundEffect={"ha"}></PianoKey>
+        <PianoKey
+          tileType={1}
+          keyToPress={"c"}
+          soundEffect={"yeahyeah"}
+        ></PianoKey>
+        <PianoKey
+          tileType={1}
+          keyToPress={"d"}
+          soundEffect={"ipullup"}
+        ></PianoKey>
+        <PianoKey
+          tileType={1}
+          keyToPress={"e"}
+          soundEffect={"nocap"}
+        ></PianoKey>
+      </div>
+      <div className={styles.blackContainer}>
+        <PianoKey
+          tileType={2}
+          keyToPress={"f"}
+          soundEffect={"youknowitsbaby"}
+        ></PianoKey>
+        <PianoKey tileType={2} keyToPress={"g"} soundEffect={"ha2"}></PianoKey>
+        <PianoKey tileType={2} keyToPress={"h"} soundEffect={"hot"}></PianoKey>
+        <PianoKey tileType={2} keyToPress={"i"} soundEffect={"okay"}></PianoKey>
+      </div>
     </div>
   );
 };
-
 export default Piano;
--- dapiano/src/Components/PianoKey/PianoKey.tsx
+++ dapiano/src/Components/PianoKey/PianoKey.tsx
@@ -13,46 +13,23 @@ interface KeyProps {
 
 const PianoKey = (props: KeyProps) => {
   let pianoOptions = {
-    volume: 0.5,
-    interrupt: false,
+    volume: 0.25,
+    interrupt: true,
   };
   const filePath: string = "/audio/" + props.soundEffect + ".mp3";
-  const [play] = useSound(filePath, { volume: 0.25 });
+  const [play] = useSound(filePath, pianoOptions);
+  const playWrapper = (event) => {
+    event.currentTarget.blur();
+    play();
+  };
   let tileStyle = styles.whiteTile;
   if (props.tileType === TileType.BLACK) {
     tileStyle = styles.blackTile;
   }
-  // const handleKeyPress = (event: any) => {
-  //   if (event.key === props.keyToPress) {
-  //     console.log(event.key, props.keyToPress);
-  //     play();
-  //   }
-  // };
-  // useEffect(() => {
-  //   console.log("running use effect");
-  //   window.addEventListener("keypress", handleKeyPress);
-  // }, []);
-
-  // window.addEventListener("keypress", (event: any) => {
-  //   if (event.key === props.keyToPress) {
-  //     console.log(event.key, props.keyToPress);
-  //     play();
-  //   }
-  // });
-  // const filePath: string = "/audio/letsgo.mp3";
-  // const [play] = useSound(filePath, { volume: 0.5 });
-  // React.useEffect(() => {
-  //   window.addEventListener("keypress", (event) => {
-  //     if (event.key === "a") {
-  //       console.log("enter press here! ");
-  //       play();
-  //     }
-  //   });
-  // }, []);
 
   return (
     <div className={styles.container}>
-      <button className={tileStyle} onClick={() => play()}>
+      <button className={tileStyle} onClick={(e) => playWrapper(e)}>
         {props.keyToPress}
       </button>
     </div>
